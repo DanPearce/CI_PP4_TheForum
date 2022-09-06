d979301f0c78b83e337f378696709c2be1ce8276
DanPearce <daniel_pearce@outlook.com>
2022-09-06 13:10:46 +0000

    Deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicon theforum.wsgi
---
+++ forumboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ForumboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'forumboard'
