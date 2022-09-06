e34c822b186b0c1d89237c21144a0ce1ac94dbf3
DanPearce <daniel_pearce@outlook.com>
2022-09-06 14:29:20 +0000

    Add model for ForumBoard

--- forumboard/models.py
+++ forumboard/models.py
@@ -4,3 +4,19 @@ Models for the ForumBoard Structure
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+class ForumBoard(models.Model):
+    """
+    This class is used to define the model paramaters for the
+    'ForumBoard'.
+    """
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=20, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    about = models.CharField(max_length=255)
+    board_background = CloudinaryField('image', default='placeholder')
+    followers = models.ManyToManyField(
+            User, related_name="board_followers", blank=True
+    )
+    approved_board = models.BooleanField(default=False)
