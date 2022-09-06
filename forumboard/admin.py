7839f8cd6d4ee3353604d83045998008c0fffbdd
DanPearce <daniel_pearce@outlook.com>
2022-09-06 15:05:37 +0000

    Add ForumBoardAdmin for admin customisation

--- forumboard/admin.py
+++ forumboard/admin.py
@@ -4,3 +4,22 @@ Admin set up for ForumBoard and it's posts/comments
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import ForumBoard, ForumPost, Comment
+
+
+@admin.register(ForumBoard)
+class ForumBoardAdmin(SummernoteModelAdmin):
+    """
+    Diplay Forum Board sections in Admin Console
+    """
+    list_display = ('name', 'slug', 'approved_board', 'created_on')
+    search_fields = ['name', 'about']
+    list_filter = ('created_on', 'approved_board')
+    prepopulated_fields = {'slug': ('name',)}
+    summernote_fields = ('about',)
+    actions = ['approve_board']
+
+    def approve_board(self, request, queryset):
+        """
+        Allows admins to approve Forum Boards for publishing
+        """
+        queryset.update(approved_board=True)
