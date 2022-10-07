82fb76cbe1e9203f44ff7b3f9287326744cf05e8
DanPearce <daniel_pearce@outlook.com>
2022-10-07 11:19:48 +0000

    Initial imports added to views.py

--- forumboard/views.py
+++ forumboard/views.py
@@ -1,3 +1,7 @@
+"""
+Views for ForumBoard
+"""
 from django.shortcuts import render
+from django.views import generic
+from .models import ForumBoard, ForumPost, Comment
 
-# Create your views here.
