2eeba388697154be4ab2239d92a0d4c035e97c59
DanPearce <daniel_pearce@outlook.com>
2022-10-08 12:36:54 +0000

    Added IndexPostView to generate a list view of posts ordered by ammount of likes

--- forumboard/views.py
+++ forumboard/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 from django.views import generic
 from .models import ForumBoard, ForumPost, Comment
 
+
+class IndexPostView(generic.ListView):
+    """
+    View to generate posts based on likes.
+    """
+    model = ForumPost
+    template_name = 'index.html'
+    queryset = ForumPost.objects.order_by('likes')
+    paginate_by = 10
