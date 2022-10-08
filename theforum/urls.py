1c851639072e16624de2d3e57a590251fab07cfd
DanPearce <daniel_pearce@outlook.com>
2022-10-08 16:27:35 +0000

    Install AllAuth, and set up the urls/settings to make this work

--- theforum/urls.py
+++ theforum/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forumboard.urls'), name='forumboard_urls'),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls'))
 ]
