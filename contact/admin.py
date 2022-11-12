60ee27bcaf6ceb2f9c79b90e178e8b8dfd3b0100
DanPearce <daniel_pearce@outlook.com>
2022-11-12 19:34:32 +0000

    Add admin view for the contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdminView(admin.ModelAdmin):
+    """
+    A view that allows the admin console to view forms sent to the contact page
+    """
+    list_display = ('created_on', 'email', 'first_name', 'last_name') 
+    search_fields = ('body', 'first_name', 'last_name', 'email')
+    list_filter = ('email', 'first_name', 'last_name')
