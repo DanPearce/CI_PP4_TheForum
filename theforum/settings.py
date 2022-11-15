f2aac296aa27db432ef2abdcffe5641b172072fa
DanPearce <daniel_pearce@outlook.com>
2022-11-15 19:59:01 +0000

    Deployment commit

--- theforum/settings.py
+++ theforum/settings.py
@@ -36,6 +36,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 # Allowed Hosts
 ALLOWED_HOSTS = ['ci-pp4-theforum-danpearce.herokuapp.com', 'localhost']
 
