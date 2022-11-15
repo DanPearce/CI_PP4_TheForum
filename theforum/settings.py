e3f08e27f1b9df2f4f4c1039319887262fd6c5d7
DanPearce <daniel_pearce@outlook.com>
2022-11-15 19:58:03 +0000

    Add error pages

--- theforum/settings.py
+++ theforum/settings.py
@@ -34,7 +34,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Allowed Hosts
 ALLOWED_HOSTS = ['ci-pp4-theforum-danpearce.herokuapp.com', 'localhost']
